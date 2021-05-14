43fd17aac161f231c704c362ee87980f075b4922
vishnu11121998 <73543827+vishnu11121998@users.noreply.github.com>
2021-05-14 12:21:53 +0530

    Add files via upload

---
+++ atm pin.py
@@ -0,0 +1,8 @@
+a = int(input("Enter pin:"))
+b = str(a)
+l = len(b)
+x=a%10
+if x==2 or x==7 and l==4:
+  print("valid")
+
+  
---
+++ diagonal.py
@@ -0,0 +1,10 @@
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+x=a%10
+y=b%10
+c=a//10
+d=b//10
+m=c+y
+n=x+d
+o=m+n
+print(o)
