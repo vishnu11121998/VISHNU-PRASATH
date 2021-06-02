051f95d3b5b0baac0362f00ed985f0470021b546
vishnu11121998 <73543827+vishnu11121998@users.noreply.github.com>
2021-06-02 12:24:41 +0530

    Update pattern31.py

--- 20 may 2021/pattern31.py
+++ 20 may 2021/pattern31.py
@@ -15,3 +15,34 @@ for i in range(0,5):
     else:
      crc=0
   print("")
+  
+  
+  #crc=0
+  #    i       j        chr(crc+65)     if crc<26        crc=crc+1
+  
+  #    0       0         A                T                 crc=1  
+  #    0       1         B                T                 crc=1  
+  #    0       2         C                T                 crc=1  
+  #    0       3         D                T                 crc=1  
+  #    0       4         E                T                 crc=1    
+  #    1       0         B                T                 crc=1   
+  #    1       1         C                T                 crc=1   
+  #    1       2         D                T                 crc=1  
+  #    1       3         E                T                 crc=1  
+  #    1       4         F                T                 crc=1  
+  #    2       0         C                T                 crc=1  
+  #    2       1         D                T                 crc=1  
+  #    2       2         E                T                 crc=1    
+  #    2       3         F                T                 crc=1   
+  #    2       4         G                T                 crc=1   
+  #    3       0         D                T                 crc=1  
+  #    3       1         E                T                 crc=1  
+  #    3       2         F                T                 crc=1  
+  #    3       3         G                T                 crc=1  
+  #    3       4         H                T                 crc=1  
+  #    4       0         E                T                 crc=1  
+  #    4       1         F                T                 crc=1    
+  #    4       2         G                T                 crc=1   
+  #    4       3         H                T                 crc=1   
+  #    4       4         I                F                 crC=0  
+  
